786cf0453b27f99bbac103a1e25733069876ad11
jgstew <james@jgstew.com>
2022-11-17 09:48:26 -0500

    reformatted with black

--- EvaluateRelevance.py
+++ EvaluateRelevance.py
@@ -13,91 +13,94 @@ import os
 import subprocess
 import sys
 
+
 def get_path_QNA():
-  test_file_paths = [
-                    "QnA",
-                    "/usr/local/bin/QnA",
-                    "/Library/BESAgent/BESAgent.app/Contents/MacOS/QnA",
-                    "/opt/BESClient/bin/QnA",
-                    "C:/Program Files (x86)/BigFix Enterprise/BES Client/qna.exe"
-                  ]
+    test_file_paths = [
+        "QnA",
+        "/usr/local/bin/QnA",
+        "/Library/BESAgent/BESAgent.app/Contents/MacOS/QnA",
+        "/opt/BESClient/bin/QnA",
+        "C:/Program Files (x86)/BigFix Enterprise/BES Client/qna.exe",
+    ]
 
-  for file_path in test_file_paths:
-    # test if path exists and is executable:
-    #   - https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-    if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
-      # return first valid path
-      ##print(file_path)
-      return file_path
+    for file_path in test_file_paths:
+        # test if path exists and is executable:
+        #   - https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            # return first valid path
+            ##print(file_path)
+            return file_path
 
-  # TODO: need to have some sort of error handling
-  return "ERROR: Valid QNA path not found!"
+    # TODO: need to have some sort of error handling
+    return "ERROR: Valid QNA path not found!"
+
 
 def EvaluateRelevance(relevance="TRUE", returntype="RAW"):
-  path_QNA = get_path_QNA()
+    path_QNA = get_path_QNA()
 
-  if not(os.path.isfile(path_QNA) and os.access(path_QNA, os.X_OK)):
-    # TODO: error handling in this case:
-    return path_QNA
+    if not (os.path.isfile(path_QNA) and os.access(path_QNA, os.X_OK)):
+        # TODO: error handling in this case:
+        return path_QNA
 
-  # There are 2 methods to eval relevance using the QNA executable
-  #   - Subprocess using FileIn(relevance), FileOut(results)
-  #     - After testing, this method has the same results parsing issues as using StdIn/StdOut
-  #     - The potential advantage to this method is if there is a character limit for StdIn/StdOut
-  #     - Can this method use filestreams instead of files? I hope so, but would need to test
-  #   - Subprocess using StdIn(relevance), StdOut(results)
-  #     - Has issues with parsing results
-  #     - Example: https://git.psu.edu/sysman/besengine/blob/master/Code/BESRelevanceProvider.py#L68
-  #   - Is there a better way using a library or an API of some sort?
+    # There are 2 methods to eval relevance using the QNA executable
+    #   - Subprocess using FileIn(relevance), FileOut(results)
+    #     - After testing, this method has the same results parsing issues as using StdIn/StdOut
+    #     - The potential advantage to this method is if there is a character limit for StdIn/StdOut
+    #     - Can this method use filestreams instead of files? I hope so, but would need to test
+    #   - Subprocess using StdIn(relevance), StdOut(results)
+    #     - Has issues with parsing results
+    #     - Example: https://git.psu.edu/sysman/besengine/blob/master/Code/BESRelevanceProvider.py#L68
+    #   - Is there a better way using a library or an API of some sort?
 
-  # There are 4 ways to return the relevance evaluation results
-  #   - Raw String of Results
-  #     - Easiest method
-  #   - Single String with separator between plural results (newline by default?)
-  #   - Array of Strings for plural results
-  #   - Hash with array of strings for results, plus timing info, return type info
+    # There are 4 ways to return the relevance evaluation results
+    #   - Raw String of Results
+    #     - Easiest method
+    #   - Single String with separator between plural results (newline by default?)
+    #   - Array of Strings for plural results
+    #   - Hash with array of strings for results, plus timing info, return type info
 
-  # How to (optionally?) return other metadata
-  #  - Timing info
-  #  - Relevance Return type (-showtypes)
-  #  - Error info
+    # How to (optionally?) return other metadata
+    #  - Timing info
+    #  - Relevance Return type (-showtypes)
+    #  - Error info
 
-  process = subprocess.Popen(
-                              [path_QNA, "-t", "-showtypes"],
-                              bufsize=-1,
-                              universal_newlines=True,
-                              stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT
-                            )
+    process = subprocess.Popen(
+        [path_QNA, "-t", "-showtypes"],
+        bufsize=-1,
+        universal_newlines=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
 
-  output_data, error_data = process.communicate(relevance)
+    output_data, error_data = process.communicate(relevance)
 
-  if error_data:
-    print("Error: " + error_data)
+    if error_data:
+        print("Error: " + error_data)
 
-  # Return raw output data:   TODO: implement other return types
-  return output_data
+    # Return raw output data:   TODO: implement other return types
+    return output_data
 
 
-def main(relevance='version of client'):
-  print(EvaluateRelevance(relevance))
+def main(relevance="version of client"):
+    print(EvaluateRelevance(relevance))
 
-if __name__ == '__main__':
-  # check for command argument, and try to pass it as the relevance to be evaluated
-  # TODO: find a better way to get the raw commandline string
-  if len(sys.argv) == 2:
-    # handle the case in which the commandline is a single string already:
-    cmd_line = sys.argv[1]
-  else:
-    # the following doesn't work in all cases, only tested on mac:
-    cmd_line = subprocess.list2cmdline(sys.argv[1:])
-    #cmdline = " ".join(map(cmd_quote, sys.argv[1:]))
 
-  if cmd_line:
-    #print len(sys.argv)
-    print("Note: this will not work on the command line directly in all cases")
-    print("Q: " + cmd_line)
-    main(cmd_line)
-  else:
-    main()
+if __name__ == "__main__":
+    # check for command argument, and try to pass it as the relevance to be evaluated
+    # TODO: find a better way to get the raw commandline string
+    if len(sys.argv) == 2:
+        # handle the case in which the commandline is a single string already:
+        cmd_line = sys.argv[1]
+    else:
+        # the following doesn't work in all cases, only tested on mac:
+        cmd_line = subprocess.list2cmdline(sys.argv[1:])
+        # cmdline = " ".join(map(cmd_quote, sys.argv[1:]))
+
+    if cmd_line:
+        # print len(sys.argv)
+        print("Note: this will not work on the command line directly in all cases")
+        print("Q: " + cmd_line)
+        main(cmd_line)
+    else:
+        main()
